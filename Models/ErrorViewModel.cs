e4e2a93e1450bebf3a8bcb26b7a436a6342e4b31
Admin <abajanillor.gmail.com>
2024-03-02 01:08:52 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PRELIM_E3_RollinJhayAbaja_BSCS32E1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
